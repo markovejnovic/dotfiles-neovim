686d413506dd3961b9bf84fd2db9b0d597581a52
Marko Vejnovic <marko.vejnovic@hotmail.com>
2024-03-23 13:36:44 -0700

    Fixed nvim-tree config

--- .config/nvim/lua/ui.lua
+++ .config/nvim/lua/ui.lua
@@ -82,14 +82,14 @@ require('lualine').setup({
 })
 
 require('nvim-tree').setup({
-  sort_by = 'case_sensitive',
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = 'u', action = 'dir_up' },
-      },
-    },
+  sort = {
+    sorter = "case_sensitive",
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
   },
 })
 
